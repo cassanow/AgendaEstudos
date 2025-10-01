2cf9b3d8ec6a8adc29775f4cf1eddb24abb65fb3
Arthur <nashcassano8@gmail.com>
2025-10-01 13:50:39 -0300

    Consumindo o endpoint Register com sucesso

---
+++ Frontend/wwwroot/js/register.js
@@ -0,0 +1,19 @@
+﻿async function register(){
+    try{
+        const name = document.getElementById('register-name').value;
+        const email = document.getElementById('register-email').value;
+        const password = document.getElementById('register-password').value;
+
+        const response = await fetch("https://localhost:7118/api/Auth/Register", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name, email, password  })
+        })
+
+        const data = await response.json();
+        console.log("registrado com sucesso", data)
+    }catch(err){
+        console.log(err)    
+    }
+       
+}
