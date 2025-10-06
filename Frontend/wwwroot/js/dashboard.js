52d2b49c87cb08d60e03952bc6565774ff7edcfb
Arthur <nashcassano8@gmail.com>
2025-10-06 13:47:57 -0300

    Criando uma pagina destinada para as tarefas e implementando css e javascript

--- Frontend/wwwroot/js/dashboard.js
+++ Frontend/wwwroot/js/dashboard.js
@@ -5,8 +5,6 @@ document.getElementById("btn-logout").addEventListener("click", function() {
     window.location.href = "/Login.html";
 })
 
-
-
 function user(){
     const userArea = document.getElementById("user-area");
     const name = localStorage.getItem('user')
@@ -14,101 +12,6 @@ function user(){
     userArea.innerHTML = name;
 }
 
-
-const modalTarefa = document.getElementById('modal-tarefa');
-const btnNovaTarefa = document.getElementById('btn-add-tarefa');
-const closeModal = document.getElementById('close-tarefa');
-btnNovaTarefa.addEventListener('click', async () => {
-    const materias = await getAllMaterias(); 
-    const materiaSelect = document.getElementById('materia-select');
-    materiaSelect.innerHTML = ""; 
-
-    materias.forEach(m => {
-        const option = document.createElement("option");
-        option.value = m.id;
-        option.textContent = m.nome;
-        materiaSelect.appendChild(option);
-    });
-
-    modalTarefa.style.display = "block";
-});
-
-closeModal.addEventListener('click', () => {
-    modalTarefa.style.display = "none";
-});
-
-window.addEventListener('click', (event) => {
-    if (event.target === modalTarefa) {
-        modalTarefa.style.display = "none";
-    }
-});
-
-document.getElementById('btn-salvar-tarefa').addEventListener('click', async function addTarefa() {
-    try{
-        const token = localStorage.getItem('token');
-        const titulo = document.getElementById('nome-tarefa').value;
-        const descricao = document.getElementById('descricao-tarefa').value;
-        const prioridade = parseInt(document.getElementById('prioridade-tarefa').value);
-        const dataInicio = document.getElementById('tarefa-data-inicio').value;
-        const dataFim = document.getElementById('tarefa-data-fim').value;
-        const materiaId = document.getElementById('materia-select').value;
-        const response = await fetch(`https://localhost:7118/api/Tarefa/AddTarefas?materiaId=${materiaId}`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
-            body: JSON.stringify({titulo, descricao, prioridade, dataInicio, dataFim})
-        });
-        
-        if (!response.ok) {
-            console.log(response);
-        }
-        
-        const data = await response.json();
-        console.log(data);
-        
-    }catch(err){
-        console.log(err);
-    }
-});
-
-
-
-async function getAllTarefas(){
-    try{
-        const token = localStorage.getItem('token');
-        const response = await fetch("https://localhost:7118/api/Tarefa/GetTarefas", {
-            method: "GET",
-            headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
-        })
-        
-        if(!response.ok) {
-            console.log("deu ruim");
-            return [];
-        }
-        else{
-            
-            return await response.json();
-        }
-    }catch(err){
-        console.log(err);
-        return [];
-    }
-}
-
-async function ListaTarefas(){
-    const tarefasContainer = document.getElementById('tarefas-container');
-    const tarefas = await getAllTarefas();
-
-    let html = "<ul>";
-    tarefas.forEach(t => {
-        html += `<li>${t.titulo}</li>`;
-    });
-    html += "</ul>";
-
-    tarefasContainer.innerHTML = html;
-}
-
-ListaTarefas();
-getAllMaterias();
 user();
 
 
---
+++ Frontend/wwwroot/js/tarefa.js
@@ -0,0 +1,123 @@
+﻿function user(){
+    const userArea = document.getElementById("user-area");
+    const name = localStorage.getItem('user')
+
+    userArea.innerHTML = name;
+}
+async function getAllMaterias(){
+    try{
+        const token = localStorage.getItem('token');
+        const response = await fetch("https://localhost:7118/Materia/GetMaterias", {
+            method: "GET",
+            headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+        })
+
+        if(!response.ok) {
+            console.log("deu ruim");
+            return [];
+        }
+        else{
+            return await response.json();
+        }
+    }catch(err){
+        console.log(err);
+        return [];
+    }
+
+}
+
+const modalTarefa = document.getElementById('modal-tarefa');
+const btnNovaTarefa = document.getElementById('btn-add-tarefa');
+const closeModal = document.getElementById('close-tarefa');
+btnNovaTarefa.addEventListener('click', async () => {
+    const materias = await getAllMaterias();
+    const materiaSelect = document.getElementById('materia-select');
+    materiaSelect.innerHTML = "";
+
+    materias.forEach(m => {
+        const option = document.createElement("option");
+        option.value = m.id;
+        option.textContent = m.nome;
+        materiaSelect.appendChild(option);
+    });
+
+    modalTarefa.style.display = "block";
+});
+
+closeModal.addEventListener('click', () => {
+    modalTarefa.style.display = "none";
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modalTarefa) {
+        modalTarefa.style.display = "none";
+    }
+});
+
+document.getElementById('btn-salvar').addEventListener('click', async function addTarefa() {
+    try{
+        const token = localStorage.getItem('token');
+        const titulo = document.getElementById('nome-tarefa').value;
+        const descricao = document.getElementById('descricao-tarefa').value;
+        const prioridade = parseInt(document.getElementById('prioridade-tarefa').value);
+        const dataInicio = document.getElementById('tarefa-data-inicio').value;
+        const dataFim = document.getElementById('tarefa-data-fim').value;
+        const materiaId = document.getElementById('materia-select').value;
+        const response = await fetch(`https://localhost:7118/api/Tarefa/AddTarefas?materiaId=${materiaId}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+            body: JSON.stringify({titulo, descricao, prioridade, dataInicio, dataFim})
+        });
+
+        if (!response.ok) {
+            console.log(response);
+        }
+
+        const modal = document.getElementById('modal-tarefa');
+        modal.style.display = "none";
+        ListaTarefas();
+
+    }catch(err){
+        console.log(err);
+    }
+});
+
+
+async function getAllTarefas(){
+    try{
+        const token = localStorage.getItem('token');
+        const response = await fetch("https://localhost:7118/api/Tarefa/GetTarefas", {
+            method: "GET",
+            headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+        })
+
+        if(!response.ok) {
+            console.log("deu ruim");
+            return [];
+        }
+        else{
+
+            return await response.json();
+        }
+    }catch(err){
+        console.log(err);
+        return [];
+    }
+}
+
+async function ListaTarefas(){
+    const tarefasContainer = document.getElementById('tarefas-container');
+    const tarefas = await getAllTarefas();
+
+    let html = "<ul>";
+    tarefas.forEach(t => {
+        html += `<li>${t.titulo}</li>`;
+    });
+    html += "</ul>";
+
+    tarefasContainer.innerHTML = html;
+}
+
+ListaTarefas();
+getAllMaterias();
+user();
